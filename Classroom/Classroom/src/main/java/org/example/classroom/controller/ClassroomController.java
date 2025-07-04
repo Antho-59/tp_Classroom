bd12e8a7c29638af42be09a504eb0e09b5c5383b
Anthoine <59anth59@gmail.com>
2025-07-04 16:30:38 +0200

    TP classroom

--- Classroom/Classroom/src/main/java/org/example/classroom/controller/ClassroomController.java
+++ Classroom/Classroom/src/main/java/org/example/classroom/controller/ClassroomController.java
@@ -1,14 +1,18 @@
 package org.example.classroom.controller;
 
 
+import org.example.classroom.dto.ClassroomReceiveDTO;
 import org.example.classroom.dto.ClassroomResponseDTO;
 import org.example.classroom.entity.Classroom;
 import org.example.classroom.service.ClassroomService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/classroom")
 public class ClassroomController {
+
+    private ClassroomService service;
 
     public ClassroomService classroomService;
     public ClassroomController(ClassroomService classroomService){
@@ -22,8 +26,9 @@ public class ClassroomController {
 
     }
 
-    @PostMapping("/create")
-    public ClassroomResponseDTO createClassroom(@RequestBody Classroom classroom){
-        return classroomService.createClassroom(classroom);
+    @PostMapping
+    public ResponseEntity<ClassroomResponseDTO> createClassroom(@RequestBody ClassroomReceiveDTO classroomReceiveDTO){
+        return ResponseEntity.status(201).body(service.createClassroom(classroomReceiveDTO.dtoToEntity()));
+
     }
 }
---
+++ Classroom/Classroom/src/main/java/org/example/classroom/repository/ClassroomRepository.java
@@ -0,0 +1,7 @@
+package org.example.classroom.repository;
+
+import org.example.classroom.entity.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+}
